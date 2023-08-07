a3f0be9b8b70b0c2b239a19b43fb94735ed762f8
charmingruby <mrrubycanal3105@gmail.com>
2023-08-07 13:44:05 -0300

    project finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
-import { BrowserRouter } from 'react-router-dom'
-import { Router } from './Router'
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/global";
+import { Router } from "./Routes";
+import { BrowserRouter } from "react-router-dom";
+import { CartContextProvider } from "./contexts/CartContext";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Router />
-    </BrowserRouter>
-  )
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <BrowserRouter>
+        <CartContextProvider>
+          <Router />
+        </CartContextProvider>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,13 @@
-export function Home() {
-  return <>home page</>
+import { Intro } from "./components/Intro";
+import { OurCoffees } from "./components/OurCoffees";
+import { HomeContainer } from "./styles";
+
+export function HomePage() {
+  return (
+    <HomeContainer>
+      <Intro />
+
+      <OurCoffees />
+    </HomeContainer>
+  );
 }
